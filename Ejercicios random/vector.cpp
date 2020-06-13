894db4bde404b6ba0eda1bda76c530433f1efe83
DevGamerLinux <64239355+DevGamerLinux@users.noreply.github.com>
2020-06-13 17:37:18 -0400

    Add files via upload

---
+++ Ejercicios random/vector.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>       // documentacion: http://www.cplusplus.com/reference/vector/vector/
+#include <algorithm>    // documentacion: 
+
+using namespace std ;
+
+vector<int> llenarVector( vector<int> );
+void mostrarVector( vector<int> );
+vector<int> ordenarMenorManyor( vector<int> );
+vector<int> ordenarManyorMenor( vector<int> );
+
+int main()
+{
+    vector<int> lista;
+
+    lista = llenarVector( lista ) ;   
+    mostrarVector( lista ) ; 
+    lista = ordenarMenorManyor( lista) ;
+    mostrarVector( lista ) ;
+    lista = ordenarManyorMenor( lista ) ;
+    mostrarVector( lista ) ;
+
+    return 0 ;
+} // fin main
+
+vector<int> llenarVector( vector<int> lista )
+{
+    lista.push_back( 1 );
+    lista.push_back( 2 );
+    lista.push_back( 3 );
+    lista.push_back( 4 );
+    lista.push_back( 5 );
+    lista.push_back( 8 );
+    lista.push_back( 9 );
+    lista.push_back( 10 );
+    lista.push_back( 6 );
+    lista.push_back( 7 );
+
+    return lista ;
+} // fin llenarVector
+
+void mostrarVector( vector<int> lista )
+{
+    for( int i = 0 ; i < lista.size() ; i++ )
+    {
+        if( i == lista.size() - 1 ) cout << lista.at(i) << endl ;
+        else cout << lista.at(i) << " " ;
+    }
+} // fin mostrarVector
+
+vector<int> ordenarMenorManyor( vector<int> lista )
+{
+    bool bandera = true ;
+    while( bandera )
+    {
+        bandera = false ;
+        for( int i = 0; i < lista.size()-1; i++) 
+        {
+            if( lista[i] > lista[i+1] )
+            {
+                lista[i] += lista[i+1] ;
+                lista[i+1] = lista[i] - lista[i+1] ;
+                lista[i] -= lista[i+1] ;
+                bandera = true ;
+            }
+        }
+    }
+    
+    return lista ;
+} // fin ordenarMenorManyor
+
+vector<int> ordenarManyorMenor( vector<int> lista )
+{
+    reverse( lista.begin() , lista.end() ) ;
+    return lista ;
+} // fin ordenarManyorMenor
